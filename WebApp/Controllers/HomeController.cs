82339517962006aa1b1224e9f06b2f12ce839a77
Leonardo Ávila <leonardo.avila98@outlook.com>
2018-10-28 13:10:02 -0300

    Migração para projeto .NET Core

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+    }
+}
