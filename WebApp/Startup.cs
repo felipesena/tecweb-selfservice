52ccd678a33d88219463307873501a6ee38331e2
Felipe Sena <felipedepaulasena@hotmail.com>
2018-11-11 20:26:00 -0200

    Mudança na ConnectionString

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -34,7 +34,7 @@ namespace WebApp
 
            
             services.AddDbContext<AplicacaoDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString(Environment.GetEnvironmentVariable("MyDbConnection")/*"DefaultConnection"*/)));
+                options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")/*"DefaultConnection"*/)));
 
             
         }
